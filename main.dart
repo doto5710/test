070ae5bc180b800c90ca53e444030e9ea9fdb309
leejinwoo <jinuwa1004@gmail.com>
2025-08-19 21:18:17 +0900

    0819 if

--- main.dart
+++ main.dart
@@ -1,8 +1,12 @@
 void main(){
-    List<String> alpabet = ['a','b','c'];
-    print(alpabet.reversed);
-    alpabet.add('d');
-    print(alpabet.reversed);
+    int number = 8;
 
+    if (number % 3 == 0) {
+    print('나머지가 0입니다');       
+    } else if(number % 3 == 1){
+        print('나머지가 1입니다.');
+    }else{
+        print('나머지가 2입니다.');
+    }
 
 }
